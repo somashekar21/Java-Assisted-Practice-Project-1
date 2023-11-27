821d67fcd91b90b98074f1a1adbb71e5c193096a
Somashekar G <Somashekargandikota@gmail.com>
2023-11-27 22:52:38 +0530

    Default Access Specifier

---
+++ src/assistedpracticeprograms/AccessModifiersDefault.java
@@ -0,0 +1,10 @@
+package assistedpracticeprograms;
+
+public class AccessModifiersDefault {
+
+	public static void main(String[] args) {
+		
+	System.out.println("This is an Example of Default Access Modifiers");
+	}
+
+}
