4aa6d79c475bf883b9c1fbe214fcf8d1cd8f59ac
Somashekar G <Somashekargandikota@gmail.com>
2023-12-06 11:07:09 +0530

    Bubble Sort

---
+++ src/Practice Programs 6th December/bubble sort/src/programs/BubbleSort.java
@@ -0,0 +1,42 @@
+package programs;
+
+public class BubbleSort {
+
+    // Function to perform bubble sort
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                // Compare adjacent elements and swap if they are in the wrong order
+                if (array[j] > array[j + 1]) {
+                    // Swap
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {64, 25, 12, 22, 11};
+
+        System.out.println("Original Array:");
+        printArray(array);
+
+        // Perform bubble sort
+        bubbleSort(array);
+
+        System.out.println("\nArray after Bubble Sort:");
+        printArray(array);
+    }
+
+    // Helper function to print an array
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
