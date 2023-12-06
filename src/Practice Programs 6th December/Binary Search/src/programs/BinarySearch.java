c2fac5f75451558238594faaa6e711701ca1dab7
Somashekar G <Somashekargandikota@gmail.com>
2023-12-06 10:46:30 +0530

    Binary search

---
+++ src/Practice Programs 6th December/Binary Search/src/programs/BinarySearch.java
@@ -0,0 +1,37 @@
+package programs;
+
+public class BinarySearch {
+
+    // Function to perform binary search
+    public static int binarySearch(int[] array, int target) {
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (array[mid] == target) {
+                return mid; // Target found, return the index
+            } else if (array[mid] < target) {
+                low = mid + 1; // Search in the right half
+            } else {
+                high = mid - 1; // Search in the left half
+            }
+        }
+
+        return -1; // Target not found
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 5, 7, 9, 11, 13, 15};
+        int target = 9;
+
+        int result = binarySearch(array, target);
+
+        if (result != -1) {
+            System.out.println("Element " + target + " found at index " + result);
+        } else {
+            System.out.println("Element " + target + " not found in the array");
+        }
+    }
+}
