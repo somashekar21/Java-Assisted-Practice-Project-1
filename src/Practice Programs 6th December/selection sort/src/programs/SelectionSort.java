87f0a0f60a563dd12bbb34dc4a00ef298b3ce8a6
Somashekar G <Somashekargandikota@gmail.com>
2023-12-06 10:58:11 +0530

    Selection Sort

---
+++ src/Practice Programs 6th December/selection sort/src/programs/SelectionSort.java
@@ -0,0 +1,46 @@
+package programs;
+
+public class SelectionSort {
+
+    // Function to perform selection sort
+    public static void selectionSort(int[] array) {
+        int n = array.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            // Find the index of the minimum element in the unsorted part
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {64, 25, 12, 22, 11};
+
+        System.out.println("Original Array:");
+        printArray(array);
+
+        // Perform selection sort
+        selectionSort(array);
+
+        System.out.println("\nArray after Selection Sort:");
+        printArray(array);
+    }
+
+    // Helper function to print an array
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
